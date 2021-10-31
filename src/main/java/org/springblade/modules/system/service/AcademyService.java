7366ee90ba0111fd8840c10e5c2cc9d7db4e091b
joverzhang <joverzh@gmail.com>
2021-10-31 21:53:22 +0800

    feat: 完善学院信息接口

--- src/main/java/org/springblade/modules/system/service/AcademyService.java
+++ src/main/java/org/springblade/modules/system/service/AcademyService.java
@@ -1,10 +1,15 @@
 package org.springblade.modules.system.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.springblade.modules.system.entity.Academy;
+import org.springblade.modules.system.query.AcademyQuery;
 
 /**
  *
  */
 public interface AcademyService extends IService<Academy> {
+
+    IPage<Academy> getPage(AcademyQuery query);
+
 }
